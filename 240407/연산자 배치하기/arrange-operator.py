2b645cb19a8c1479a3b7fd6e426b241582f9c136
sungchan Yeo <sungchan254@naver.com>
2024-04-07 00:47:33 +0900

    Title: 연산자 배치하기, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240407/연산자 배치하기/arrange-operator.py
@@ -0,0 +1,29 @@
+from sys import stdin as s
+
+def dfs(index: int, value: int, numbers: list, operands: list) -> list[int, int]:
+    if sum(operands) == 0:
+        return [value]
+    result = []
+    # 덧셈
+    if operands[0] > 0:
+        copy_operands = operands[:]
+        copy_operands[0] -= 1
+        result += dfs(index+1, value + numbers[index], numbers,copy_operands)
+    # 뺄셈
+    if operands[1] > 0:
+        copy_operands = operands[:]
+        copy_operands[1] -= 1
+        result += dfs(index+1, value - numbers[index], numbers, copy_operands)
+    # 곱셈
+    if operands[2] > 0:
+        copy_operands = operands[:]
+        copy_operands[2] -= 1
+        result += dfs(index+1, value * numbers[index], numbers,copy_operands )
+
+    result.sort()    
+    return result
+
+n = int(s.readline())
+numbers = list(map(int, s.readline().split()))
+operands = list(map(int, s.readline().split()))
+print(" ".join(map(str, dfs(1, numbers[0], numbers, operands))))
