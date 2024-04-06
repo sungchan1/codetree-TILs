6e6a03868b129b8974769013401a534db4f2b74a
sungchan Yeo <sungchan254@naver.com>
2024-04-07 01:34:43 +0900

    Title: 조삼모사, Time: 3822ms, Memory: 29MB, Status: Passed - CodeTree

--- 240407/조삼모사/three-at-dawn-and-four-at-dusk.py
+++ 240407/조삼모사/three-at-dawn-and-four-at-dusk.py
@@ -1,48 +1,64 @@
-def get_work_balance(morning_works: list, works: list):
-    morning_work_time = 0
-    evening_work_time = 0
+import sys
 
+INT_MAX = sys.maxsize
 
-    
-    for row in range(len(works)):
-        for col in range(len(works[0])):
-            if row == col :
-                continue
-            work_time = works[row][col]
-            if morning_works[row] and morning_works[col]: 
-                morning_work_time += work_time
-            elif not morning_works[row] and not morning_works[col]:
-                evening_work_time += work_time
-
-    return abs(morning_work_time-evening_work_time)
-
-
-def dfs(day: int, morning_works: list, works: list):
-    # 균등하게 일을 나눴을때 
-    if morning_works.count(True) == morning_works.count(False):
-        return get_work_balance(morning_works, works)
-
-    elif day == len(works) :
-        return float("inf")
-
-
-    # work morning_works
-    morning_works[day] = True
-    answer = dfs(day+1, morning_works, works)
-
-    # work evening
-    morning_works[day] = False
-    answer = min(answer, dfs(day+1, morning_works, works))
-    return answer
-    
-# 입력
+# 변수 선언 및 입력
 n = int(input())
-works = []
-for row in range(n):
-    works.append(list(map(int, input().split())))
+p = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+evening = [
+    False for _ in range(n)
+]
 
-# works 에 넣어요
+ans = INT_MAX
 
-# 아침에 무슨 일을 할건지
-mornig_flag = [False] * n 
-print(dfs(0, mornig_flag, works))
+
+# 아침과 저녁 간의 힘듦의 차이를 계산합니다.
+def calc():
+    morning_sum = sum([
+        p[i][j]  
+        for i in range(n)
+        for j in range(n)
+        if not evening[i] and not evening[j]
+    ])
+    evening_sum = sum([
+        p[i][j]  
+        for i in range(n)
+        for j in range(n)
+        if evening[i] and evening[j]
+    ])
+        
+    return abs(morning_sum - evening_sum)
+
+
+def find_min(curr_idx, cnt):
+    global ans
+    
+    # 정확히 아침 / 저녁으로 n / 2개씩 일이 나뉜 경우에만
+    if cnt == n // 2:
+        # 선택된 조합에 대해 합의 차이를 계산하여
+        # 그 중 최솟값을 찾습니다.
+        ans = min(ans, calc())
+        return
+    
+    # n개에 대해 전부 결정했음에도
+    # n / 2 개로 나뉘지 못한 경우라면
+    # 바로 퇴각합니다.
+    if curr_idx == n:
+        return
+
+    # curr_idx 번째 업무를 아침에 하는 경우
+    find_min(curr_idx + 1, cnt)
+    
+    # curr_idx 번째 업무를 저녁에 하는 경우
+    evening[curr_idx] = True
+    find_min(curr_idx + 1, cnt + 1)
+    evening[curr_idx] = False
+
+
+find_min(0, 0)
+
+# 출력:
+print(ans)
