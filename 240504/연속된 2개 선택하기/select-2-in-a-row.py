02bf218146201c943343b47d4a3e664c2baf2e53
sungchan Yeo <sungchan254@naver.com>
2024-05-04 18:44:13 +0900

    Title: 연속된 2개 선택하기, Time: 152ms, Memory: 25MB, Status: Passed - CodeTree

--- 240504/연속된 2개 선택하기/select-2-in-a-row.py
+++ 240504/연속된 2개 선택하기/select-2-in-a-row.py
@@ -5,19 +5,16 @@ def max_sum_with_constraints(arr):
     if n == 1:
         return arr[0]
     if n == 2:
-        return sum(arr[0], arr[1])
+        return arr[0] + arr[1]  # 두 개만 있을 경우, 두 개 모두 더하는 것이 최대값
     
     # DP 배열 초기화
     dp = [0] * (n + 1)
     dp[1] = arr[0]
-    dp[2] = max(arr[0], arr[1])
-    
-    # n이 3 이상일 때 3번째 원소부터 계산 시작
-    if n > 2:
-        dp[3] = max(dp[2], dp[1] + arr[2], arr[0] + arr[1])
+    dp[2] = arr[0] + arr[1]
+    dp[3] = max(arr[0] + arr[1], arr[1] + arr[2], arr[0] + arr[2])
     
     for i in range(4, n + 1):
-        dp[i] = max(dp[i-1], dp[i-2] + arr[i-1], dp[i-3] + arr[i-2] + arr[i-1])
+        dp[i] = max(dp[i-1], arr[i-1] + dp[i-2], arr[i-1] + arr[i-2] + dp[i-3])
     
     return dp[n]
 
