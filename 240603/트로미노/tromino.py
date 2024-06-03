ddae0b7019721a5a748519b2dcb0a954a9a5850c
sungchan Yeo <sungchan254@naver.com>
2024-06-03 10:21:44 +0900

    Title: 트로미노, Time: 203ms, Memory: 26MB, Status: Passed - CodeTree

--- 240603/트로미노/tromino.py
+++ 240603/트로미노/tromino.py
@@ -1,6 +1,12 @@
-N, M = map(int, input().split())
+from sys import stdin as s
+
+# s = open("input.txt", "r")
+
+
+
+N, M = map(int, s.readline().split())
 numbers = [
-    list(map(int, input().split()))
+    list(map(int, s.readline().split()))
     for _ in range(N)
 ]
 
@@ -13,8 +19,10 @@ for row in range(N):
         if row+2 < N:
             result = max(result, sum([numbers[row][col], numbers[row+1][col], numbers[row+2][col]]))
         # horizontal
-        if col+2 < N:
+        if col+2 < M:
             result = max(result, sum(numbers[row][col:col+3]))
+            # print(f"{row}, {col}, {sum(numbers[row][col:col+3])}")
+            
     # tromino
         if row+1 < N and col+1 < M:
             result = max(result, sum([numbers[row][col], numbers[row+1][col], numbers[row][col+1]]))
